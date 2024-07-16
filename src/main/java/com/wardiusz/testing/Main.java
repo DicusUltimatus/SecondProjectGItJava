a0fc075378aec334accb14d7ca905804e7441740
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2024-07-16 17:31:56 +0200

    Initial Commit

--- src/main/java/com/wardiusz/testing/Main.java
+++ src/main/java/com/wardiusz/testing/Main.java
@@ -2,6 +2,7 @@ package com.wardiusz.testing;
 
 public class Main {
     public static void main(String[] args) {
+        new test();
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/com/wardiusz/testing/test.java
@@ -0,0 +1,7 @@
+package com.wardiusz.testing;
+
+public class test {
+    public test() {
+        System.out.println("Poggers!");
+    }
+}
